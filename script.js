658e03e1f82eebb7a27c70c8e6004bb20f34a72b
OliviaGeist <114550562+OliviaGeist@users.noreply.github.com>
2026-05-14 18:02:43 +0100

    Create basic calculator logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function addition(a, b) {
+    console.log("add");
     return a + b;
 };
 
@@ -14,9 +15,17 @@ function division(a, b) {
     return a / b;
 };
 
-let numA = 0;
+let numA = "";
 let operator = "";
-let numB = 0;
+let numB = "";
+
+let numAOpen = true;
+let operatorOpen = false;
+let numBOpen = false;
+
+const digitButtons = document.querySelectorAll(".digit-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+const display = document.querySelector("#display");
 
 function operate(a, o, b) {
     switch (o) {
@@ -34,3 +43,71 @@ function operate(a, o, b) {
         break;
     };
 };
+
+function updateFirstNumber(e) {
+    if (numAOpen) {
+        numA = +(numA + e.target.id);
+        display.textContent = numA;
+        operatorOpen = true;
+        console.log(`numA is ${numA}`);
+        console.log(`operator is ${operator}`);
+        console.log(`numB is ${numB}`);
+    };
+};
+
+function getFirstNumber() {
+    digitButtons.forEach((digitButton) => {
+        digitButton.addEventListener("click", event => updateFirstNumber(event));
+    });
+};
+
+function updateOperator(e) {
+    if (operatorOpen) {
+        if (numA && operator && numB) {
+            const answer = operate(numA, operator, numB);
+            console.log(answer);
+            display.textContent = answer;
+            numA = answer;
+        };
+        numB = "";
+        numAOpen = false;
+        numBOpen = false;
+        if (e.target.id !== "=") {
+            operator = e.target.id;
+            operatorOpen = false;
+            numBOpen = true;
+        }
+        console.log(`numA is ${numA}`);
+        console.log(`operator is ${operator}`);
+        console.log(`numB is ${numB}`);
+
+    };
+};
+
+function getOperator() {
+    operatorButtons.forEach((operatorButton) => {
+        operatorButton.addEventListener("click", event => updateOperator(event));
+    });
+};
+
+function updateSecondNumber(e) {
+    if (numBOpen) {
+        numB = +(numB + e.target.id);
+        display.textContent = numB;
+        operatorOpen = true;
+        getOperator();
+        console.log(`numA is ${numA}`);
+        console.log(`operator is ${operator}`);
+        console.log(`numB is ${numB}`);
+    };
+};
+
+function getSecondNumber() {
+    digitButtons.forEach((digitButton) => {
+        digitButton.addEventListener("click", event => updateSecondNumber(event));
+    });
+};
+
+getFirstNumber();
+getSecondNumber();
+getOperator();
